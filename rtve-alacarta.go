407982dfcf6e92abc41bf86e5477553780c18f53
Ignacio Torres Masdeu <i@itorres.net>
2014-10-21 12:23:56 +0200

    Great version 0.2. Algorithm works
    FIXED: url algorithm.
    - I was using Base64 URL Encoded and the RTVE code didn't. This was the
      great error.
    - Also the RTVE code uses UTC dates rounded to seconds due to it parsing
      a string with seconds resolution.
    
    ALSO: Some convenience functions to do tests

--- rtve-alacarta.go
+++ rtve-alacarta.go
@@ -12,6 +12,7 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -23,8 +24,12 @@ import (
 	"time"
 )
 
-const downloadDir string = "/nas/3TB/Media/In/rtve/d"
-const cacheDir string = "/nas/3TB/Media/In/rtve/d/.cache"
+var dirs map[string]string = map[string]string{
+	"base":     "/nas/3TB/Media/In/rtve/",
+	"download": "/nas/3TB/Media/In/rtve/d",
+	"cache":    "/nas/3TB/Media/In/rtve/cache",
+	"log":      "/nas/3TB/Media/In/rtve/log",
+}
 
 func stripchars(str, chr string) string {
 	return strings.Map(func(r rune) rune {
@@ -56,6 +61,7 @@ type Episode struct {
 	Private struct {
 		URL    string
 		Offset int
+		Size   int64
 	}
 	Qualities []EpisodeFile
 }
@@ -72,10 +78,12 @@ type Programas struct {
 	}
 }
 
-func makeCacheDir() {
-	err := os.MkdirAll("/nas/3TB/Media/In/rtve/d/.cache", 0755)
-	if err != nil {
-		log.Fatal(err)
+func makeDirs() {
+	for _, dir := range dirs {
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
@@ -105,7 +113,8 @@ func cryptaes(text, key string) string {
 	}
 
 	str := []byte(text)
-	iv := []byte("\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000")
+	var a [16]byte
+	iv := a[:]
 
 	encrypter := cipher.NewCBCEncrypter(ckey, iv)
 
@@ -114,14 +123,7 @@ func cryptaes(text, key string) string {
 
 	encrypter.CryptBlocks(out, str)
 
-	base64Out := base64.URLEncoding.EncodeToString(out)
-
-	decrypter := cipher.NewCBCDecrypter(ckey, iv)
-	base64In, _ := base64.URLEncoding.DecodeString(base64Out)
-	in := make([]byte, len(base64In))
-	decrypter.CryptBlocks(in, base64In)
-
-	in = UnPKCS7Padding(in)
+	base64Out := base64.StdEncoding.EncodeToString(out)
 	return base64Out
 }
 
@@ -130,12 +132,18 @@ func orfeo(id int, t int64) string {
 	secret := fmt.Sprintf("%d_es_%d", id, t)
 	orfeo := cryptaes(secret, mobilekey)
 	return "http://www.rtve.es/ztnr/consumer/orfeo/video/" + orfeo
+}
 
+func oceano(id int, t int64) string {
+	tabletkey := "pmku579tg465GDjf1287gDFFED56788C"
+	secret := fmt.Sprintf("%d_es_%d", id, t)
+	oceano := cryptaes(secret, tabletkey)
+	return "http://www.rtve.es/ztnr/consumer/oceano/video/" + oceano
 }
 
 func cacheFile(url string) string {
 	file := fmt.Sprintf("%x", sha256.Sum256([]byte(url)))
-	path := path.Join("/nas/3TB/Media/In/rtve/d/.cache", file)
+	path := path.Join(dirs["cache"], file)
 	return path
 }
 
@@ -182,15 +190,22 @@ func (e *Programas) get(programid int) {
 	log.Println("Tenemos episodios de", e.Page.Items[0].ProgramInfo.Title)
 }
 
-func (e *Episode) remote(offset int) int {
-	t := time.Now().Local().Add(time.Duration(offset) * time.Second)
+func (e *Episode) remote(offset int, doOceano bool) int {
+	t := time.Now().UTC().Round(time.Second).Add(time.Duration(offset) * time.Second)
 	ts := t.UnixNano() / int64(time.Millisecond)
-	videourl := orfeo(e.Id, ts)
+	var videourl string
+	if doOceano {
+		videourl = oceano(e.Id, ts)
+	} else {
+		videourl = orfeo(e.Id, ts)
+	}
+
 	res, err := http.Head(videourl)
 	if err != nil {
 		log.Fatal(err)
 	}
 	if res.StatusCode == 200 {
+		e.Private.Size = res.ContentLength
 		e.Private.URL = videourl
 		e.Private.Offset = offset
 	}
@@ -198,58 +213,103 @@ func (e *Episode) remote(offset int) int {
 }
 
 func (e *Episode) writeData() {
-	b, err := json.Marshal(e)
+	b, err := json.MarshalIndent(e, "", "  ")
 	if err != nil {
 		fmt.Println("error:", err)
 	}
 	filename := fmt.Sprintf("%d.json", e.Id)
-	err = ioutil.WriteFile(path.Join("/nas/3TB/Media/In/rtve/d", filename), b, 0644)
+	err = ioutil.WriteFile(path.Join(dirs["download"], filename), b, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func (e *Episode) stat() {
+	if !e.statOceano(true) {
+		e.statOceano(false)
+	}
+}
+func (e *Episode) statOceano(doOceano bool) bool {
 
-	for i := 89400; i < 90600; i = i + 15 {
-		r := e.remote(i)
+	for i := 0; i < 1000; i = i + 20 {
+
+		r := e.remote(i, doOceano)
 		if r == 200 {
-			log.Println(">", e)
-			return
+			log.Println(i, ">", e)
+			return true
 		}
-		r = e.remote(i - 90000)
+		r = e.remote(i+3600, doOceano) // UTC+1
 		if r == 200 {
 			log.Println(">", e)
-			return
+			return true
 		}
-		r = e.remote(i - 120000)
+
+		r = e.remote(i+7200, doOceano) // UTC+2
+		if r == 200 {
+			log.Println(i, ">", e)
+			return true
+		}
+
+		r = e.remote(i+60000, doOceano) // Fuzzing val
 		if r == 200 {
 			log.Println(">", e)
-			return
+			return true
+		}
+		r = e.remote(i+30000, doOceano) // Fuzz
+		if r == 200 {
+			log.Println(">", e)
+			return true
+		}
+		r = e.remote(i+90000, doOceano) // Fuzz
+		if r == 200 {
+			log.Println(">", e)
+			return true
 		}
 	}
 	log.Println("x", e)
+	return false
 }
 
 func (e *Episode) download() {
 	filename := fmt.Sprintf("%d.mp4", e.Id)
-	filename = path.Join(downloadDir, filename)
+	filename = path.Join(dirs["download"], filename)
 
 	fi, err := os.Stat(filename)
 	if err != nil && !os.IsNotExist(err) {
 		log.Fatal(err)
 	}
-	if !os.IsNotExist(err) && (fi.Size() == e.Qualities[0].Filesize || fi.Size() == e.Qualities[1].Filesize) {
-		log.Println("> Sile", e)
-		return
+
+	if !os.IsNotExist(err) {
+		if fi.Size() >= e.Private.Size && e.Qualities != nil && (fi.Size() == e.Qualities[0].Filesize || fi.Size() == e.Qualities[1].Filesize) {
+			// Our file is bigger and canonical
+			fmt.Fprintln(os.Stdout, err, "> Sile", fi.Size(), e.Private.Size)
+			return
+		}
+
+		if fi.Size() < e.Private.Size {
+			if e.Qualities != nil && (e.Private.Size == e.Qualities[0].Filesize || e.Private.Size == e.Qualities[1].Filesize) {
+				log.Println("Better version of", e.Id, fi.Size(), "available. Remote size:", e.Private.Size)
+				return
+			} else {
+				// There's a greater size available but it's not listed. Better mak a backup of the local file.
+				log.Println("Larger NOT CANONICAL version of", e.Id, fi.Size(), "available. Remote size:", e.Private.Size)
+				log.Println("Backing up", filename, "to", filename+".bak")
+				err = os.Rename(filename, filename+".bak")
+				if err != nil {
+					fmt.Println("Error moving", filename, "to", filename+".bak", err)
+					return
+				}
+			}
+		}
 	}
 
-	output, err := os.Create(filename)
+	output, err := os.Create(filename + ".temp")
 	if err != nil {
 		fmt.Println("Error while creating", filename, "-", err)
 		return
 	}
 	defer output.Close()
+	log.Println("Downloading", e.Id, e.Private.URL)
 
 	response, err := http.Get(e.Private.URL)
 	if err != nil {
@@ -264,9 +324,86 @@ func (e *Episode) download() {
 		return
 	}
 	fmt.Println(n, "bytes downloaded.")
+	err = os.Rename(filename+".temp", filename)
+	if err != nil {
+		fmt.Println("Error moving", filename+".temp", "to", filename, err)
+		return
+	}
+
+}
+func setupLog() *os.File {
+	t, _ := time.Now().Truncate(time.Hour).MarshalText()
+	ts := string(t[:])
+
+	filename := fmt.Sprintf("%s.log", ts)
+	logfile := path.Join(dirs["log"], filename)
+	f, err := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	log.SetFlags(log.LstdFlags)
+	log.SetOutput(io.MultiWriter(f, os.Stdout))
+	return f
+
+}
+
+func (e *Episode) fromFile(f string) {
+	content, err := ioutil.ReadFile(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(content, e)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func indexFiles() {
+	log.Println("Believe it or not I'm reindexing")
+	dirfiles, err := ioutil.ReadDir(dirs["download"])
+	if err != nil {
+		log.Fatalf("error reading dir: %v", err)
+	}
+
+	for _, file := range dirfiles {
+		if path.Ext(file.Name()) == ".json" {
+			var e Episode
+			e.fromFile(path.Join(dirs["download"], file.Name()))
+			fmt.Println(file.Name(), e.Id, e.Private.Size)
+			// Episode debería tener las funciones de comprobar integridad
+		}
+	}
+}
+
+func test() {
+	// 2808202
+	var e Episode
+	e.fromFile(path.Join(dirs["download"], "2808202.json"))
+	e.stat()
+	e.writeData()
+	fmt.Println(e.Id, e.Private.Size, e)
+	e.download()
+
 }
 func main() {
-	makeCacheDir()
+	setupLog()
+	dotest := false
+	doindex := false
+	flag.BoolVar(&doindex, "i", false, "reindex the whole thing")
+	flag.BoolVar(&dotest, "t", false, "test algorithms")
+	flag.Parse()
+	if dotest {
+		test()
+		return
+	}
+	if doindex {
+		indexFiles()
+		return
+	}
+	makeDirs()
+
 	log.Println("marchando")
 	programids := []int{
 		80170, // Pokemon XY
@@ -281,8 +418,8 @@ func main() {
 		p.get(v)
 		for _, e := range p.Page.Items {
 			e.stat()
-			e.writeData()
-			e.download()
+			// e.writeData()
+			//e.download()
 		}
 	}
 }
